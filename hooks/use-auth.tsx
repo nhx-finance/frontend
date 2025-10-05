86828c15d167bb02cb999e3d5037b198b7c7d376
divin3circle <omutelema.silas21@students.dkut.ac.ke>
2025-10-05 17:43:00 +0300

    fix: change initial authentication state to true in useAuth hook

--- hooks/use-auth.tsx
+++ hooks/use-auth.tsx
@@ -1,7 +1,7 @@
 import { useState } from "react";
 
 export const useAuth = () => {
-  const [isAuthenticated, setIsAuthenticated] = useState(false);
+  const [isAuthenticated, setIsAuthenticated] = useState(true);
 
   return { isAuthenticated };
 };
