7cc79f25547926bcab82d1a93935838da4643125
divin3circle <omutelema.silas21@students.dkut.ac.ke>
2025-10-13 07:28:49 +0300

    feat: enhance stock detail page with sell functionality; integrate SellToken component and improve trade action selection with responsive drawer UI

--- app/dashboard/stocks/sell-token.tsx
+++ app/dashboard/stocks/sell-token.tsx
@@ -12,18 +12,20 @@ import "./styles.css";
 import { ChevronDown } from "lucide-react";
 import { KES_USDC_EXCHANGE_RATE } from "./swap-tokens";
 
-function BuyToken({ stock }: { stock: Stock }) {
-  const [buyValue, setBuyValue] = useState("0");
-  const [payWith, setPayWith] = useState("KES");
+const tokenBalance = 7.42;
+
+function SellToken({ stock }: { stock: Stock }) {
+  const [sellValue, setSellValue] = useState("0");
+  const [sellTo, setSellTo] = useState("KES");
 
   const { fontSize, textRef } = useDynamicFontSize({
-    value: buyValue,
+    value: sellValue,
     maxFontSize: 48,
     minFontSize: 24,
   });
 
   const handleBuyInput = (e: React.ChangeEvent<HTMLInputElement>) => {
-    setBuyValue(e.target.value);
+    setSellValue(e.target.value);
   };
 
   return (
@@ -31,22 +33,22 @@ function BuyToken({ stock }: { stock: Stock }) {
       <div className="rounded-3xl border border-foreground/20 p-4 h-[250px] flex flex-col justify-between">
         <div className="flex items-center justify-between">
           <p className="text-sm font-funnel-display font-light text-muted-foreground">
-            You are Buying
+            You are Selling
           </p>
           <div className="flex items-center gap-1">
             <Popover>
               <PopoverTrigger className="cursor-pointe border border-foreground/20 rounded-3xl px-2 py-1 flex items-center gap-1">
                 <p className="text-xs font-funnel-display font-light text-muted-foreground">
-                  {payWith}
+                  {sellTo}
                 </p>
                 <ChevronDown className="w-4 h-4 text-muted-foreground" />
               </PopoverTrigger>
               <PopoverContent className="flex flex-col w-[200px] shadow-none">
                 <h1 className="text-xs font-funnel-display font-light text-muted-foreground">
-                  Pay With
+                  Receive
                 </h1>
                 <div
-                  onClick={() => setPayWith("USD")}
+                  onClick={() => setSellTo("USD")}
                   className="flex items-center gap-1 cursor-pointer hover:bg-foreground/5 ease-in duration-300 transition-all p-2 rounded-xl"
                 >
                   <span>🇺🇸</span>
@@ -55,7 +57,7 @@ function BuyToken({ stock }: { stock: Stock }) {
                   </p>
                 </div>
                 <div
-                  onClick={() => setPayWith("KES")}
+                  onClick={() => setSellTo("KES")}
                   className="flex items-center gap-1 cursor-pointer hover:bg-foreground/5 ease-in duration-300 transition-all p-2 rounded-xl"
                 >
                   <span>🇰🇪</span>
@@ -71,7 +73,7 @@ function BuyToken({ stock }: { stock: Stock }) {
           <input
             ref={textRef}
             type="number"
-            value={buyValue}
+            value={sellValue}
             onChange={handleBuyInput}
             className="w-full h-full text-center bg-transparent placeholder:text-muted-foreground focus:outline-none font-funnel-display no-spinners-input"
             style={{ fontSize: `${fontSize}px` }}
@@ -80,47 +82,62 @@ function BuyToken({ stock }: { stock: Stock }) {
         </div>
         <div className="flex items-center justify-center gap-2">
           <div
-            onClick={() => setBuyValue("100")}
+            onClick={() => setSellValue((tokenBalance * 0.25).toFixed(2))}
             className="flex items-center justify-center px-4 py-2 rounded-3xl border border-foreground/20 cursor-pointer hover:bg-foreground/5 ease-in duration-300 transition-all"
           >
             <p className="text-xs font-funnel-display font-light text-muted-foreground">
-              {payWith === "KES" ? "KES" : "$"} 100
+              25%
             </p>
           </div>
           <div
-            onClick={() => setBuyValue("500")}
+            onClick={() => setSellValue((tokenBalance * 0.5).toFixed(2))}
             className="flex items-center justify-center px-4 py-2 rounded-3xl border border-foreground/20 cursor-pointer hover:bg-foreground/5 ease-in duration-300 transition-all"
           >
             <p className="text-xs font-funnel-display font-light text-muted-foreground">
-              {payWith === "KES" ? "KES" : "$"} 500
+              50%
             </p>
           </div>
           <div
-            onClick={() => setBuyValue("1000")}
+            onClick={() => setSellValue((tokenBalance * 0.75).toFixed(2))}
             className="flex items-center justify-center px-4 py-2 rounded-3xl border border-foreground/20 cursor-pointer hover:bg-foreground/5 ease-in duration-300 transition-all"
           >
             <p className="text-xs font-funnel-display font-light text-muted-foreground">
-              {payWith === "KES" ? "KES" : "$"} 1,000
+              75%
+            </p>
+          </div>
+          <div
+            onClick={() => setSellValue(tokenBalance.toFixed(2))}
+            className="flex items-center justify-center px-4 py-2 rounded-3xl border border-foreground/20 cursor-pointer hover:bg-foreground/5 ease-in duration-300 transition-all"
+          >
+            <p className="text-xs font-funnel-display font-light text-muted-foreground">
+              Max
             </p>
           </div>
         </div>
       </div>
       <div className="bg-foreground/5 rounded-3xl h-[80px] hover:bg-foreground/10 ease-in duration-300 transition-all my-2 flex items-center justify-between p-4 cursor-pointer">
-        <div className="flex items-center gap-1">
+        <div className="flex items-start gap-1 flex-col ">
+          <p className="text-sm font-funnel-display font-light text-muted-foreground">
+            {tokenBalance.toLocaleString(undefined, {
+              minimumFractionDigits: 2,
+              maximumFractionDigits: 2,
+            })}{" "}
+            {stock.ticker}
+          </p>
           <p className="text-sm font-funnel-display font-semibold">
-            {payWith === "KES"
-              ? (Number(buyValue) / stock.price).toLocaleString(undefined, {
+            {sellTo === "KES"
+              ? (Number(sellValue) * stock.price).toLocaleString(undefined, {
                   minimumFractionDigits: 2,
                   maximumFractionDigits: 2,
                 })
               : (
-                  (Number(buyValue) * KES_USDC_EXCHANGE_RATE) /
+                  (Number(sellValue) / KES_USDC_EXCHANGE_RATE) *
                   stock.price
                 ).toLocaleString(undefined, {
                   minimumFractionDigits: 2,
                   maximumFractionDigits: 2,
                 })}{" "}
-            {stock.ticker}
+            {sellTo === "KES" ? "KES" : "USDC"}
           </p>
         </div>
 
@@ -133,7 +150,7 @@ function BuyToken({ stock }: { stock: Stock }) {
             className="rounded-xl object-cover"
           />
           <div className="">
-            <p className="text-lg font-semibold font-funnel-display">
+            <p className="text-lg font-semibold font-funnel-display text-muted-foreground">
               {stock.ticker}
             </p>
             <p className="text-xs font-funnel-display font-light text-muted-foreground">
@@ -146,4 +163,4 @@ function BuyToken({ stock }: { stock: Stock }) {
   );
 }
 
-export default BuyToken;
+export default SellToken;
