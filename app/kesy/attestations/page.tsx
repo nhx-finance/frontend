e01b7c97c2ce7620161c8e172737ab5337630776
divin3circle <omutelema.silas21@students.dkut.ac.ke>
2025-11-25 15:29:10 +0300

    blogs page attestations page

--- app/kesy/attestations/page.tsx
+++ app/kesy/attestations/page.tsx
@@ -1,11 +1,25 @@
 import React from "react";
 import Navbar from "@/components/kesy/navbar";
+import Link from "next/link";
 
 function Attestations() {
   return (
-    <div>
+    <div className="max-w-7xl mx-auto">
       <Navbar />
-      <h1 className="text-2xl font-bold">Attestations</h1>
+      <h1 className="text-2xl font-semibold font-funnel-display mt-10">
+        Attestations
+      </h1>
+      <p className="text-sm font-funnel-display text-center mt-4 text-muted-foreground">
+        No attestations available yet
+      </p>
+      <div className="w-full flex items-center justify-center">
+        <Link
+          href="/kesy"
+          className="text-sm font-funnel-display font-bold text-center mt-10 underline"
+        >
+          Back to Home
+        </Link>
+      </div>
     </div>
   );
 }
--- app/kesy/blogs/[id]/page.tsx
+++ app/kesy/blogs/[id]/page.tsx
@@ -1,12 +1,23 @@
 "use client";
 import { useParams } from "next/navigation";
 import React from "react";
+import Navbar from "@/components/kesy/navbar";
+import Link from "next/link";
 
 function BlogDetailsPage() {
   const { id } = useParams();
   return (
-    <div>
-      <h1>Blog Details {id}</h1>
+    <div className="max-w-7xl mx-auto flex items-center flex-col justify-between">
+      <Navbar />
+      <p className="text-sm font-funnel-display font-bold text-center mt-10 text-muted-foreground">
+        Blog coming soon
+      </p>
+      <Link
+        href="/kesy/blogs"
+        className="text-sm font-funnel-display font-bold text-center mt-10 underline"
+      >
+        Back to Blogs
+      </Link>
     </div>
   );
 }
